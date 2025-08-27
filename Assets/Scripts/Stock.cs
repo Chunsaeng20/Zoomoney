ba4a8e8f28f9b56d97de83371080a675c1abc2f9
dbstjr3067 <dbstjr3067@gmail.com>
2025-08-27 23:16:13 +0900

    Stock class added

--- Assets/Scripts/Stock.cs
+++ Assets/Scripts/Stock.cs
@@ -1,16 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+public enum Sectors
+{
+    IT = 0,
+    Retail,
+    Entertainment
+}
+public class StockInformation
+{
+    public string ID; //주식 이름
+    public Sectors Sector; //섹터(분야)
+    public float PriorStockPrice; //이전 가격
+    public float CurrentStockPrice; //현재 가격
+}
 public class Stock : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<StockInformation> stocks;
+    public void StockPriceFluctation(List<float> Cur)//게임매니저 클래스에서 받은 변동폭으로 주가 변동
     {
-        
+        for (int i = 0; i < Cur.Count; i++)
+        {
+            stocks[i].PriorStockPrice = stocks[i].CurrentStockPrice;
+            stocks[i].CurrentStockPrice = Cur[i];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NewStockPrice(string id, float newStockPrice)
     {
-        
+
+    }
+
+    public List<StockInformation> CurrentStocks()//현재 주식리스트를 게임매니저 클래스에 반환
+    {
+        return stocks;
     }
 }
