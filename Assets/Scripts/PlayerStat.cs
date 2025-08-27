83c185eb05d2b20d972d7e82e854db57fd9e997c
jijiho <kaleido429@gmail.com>
2025-08-28 08:08:48 +0900

    staminaIncrease 변수 설정

--- Assets/Scripts/PlayerStat.cs
+++ Assets/Scripts/PlayerStat.cs
@@ -8,16 +8,18 @@ public class PlayerStat : MonoBehaviour
     public Trader traderManager;
     public void CompanyDinner()
     {
+        int staminaIncrease = 1;
         foreach (TraderInfo trader in traderManager.traderList)
         {
             // 만약 트레이더가 고용(isHire)되었고, 투자에 참여(isParticipate)했다면
+            // -> 현재 턴에 내 덱에 있는 트레이더들
             if (trader.isHire && trader.isParticipate)
             {
                 // 해당 트레이더의 체력(stamina)을 1 증가시킵니다.
-                trader.stamina += 1;
+                trader.stamina += staminaIncrease;
 
                 // 콘솔에 변경된 내용을 출력하여 잘 작동하는지 확인합니다. (선택 사항)
-                Debug.Log(trader.traderName + "의 체력이 1 증가했습니다. 현재 체력: " + trader.stamina);
+                Debug.Log(trader.stamina);
             }
         }
     }
