b3cc852f95250d6d6542ad4d57b55073a3fa4aaf
함대영 <87267361+HolicKW@users.noreply.github.com>
2025-08-28 15:55:27 +0900

    Update
    update

--- Assets/Scripts/MoveCard.cs
+++ Assets/Scripts/MoveCard.cs
@@ -1,14 +1,35 @@
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveCard : MonoBehaviour
 {
 
-    public GameManager gameManager;
+    public GameObject GO;
+    public Trader trader;
+    public GameObject[] cardPrefab;
+    public Transform parentObject;
     [SerializeField] public List<TraderInfo> LeftCards;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        LeftCards = gameManager.trader.traderList;
+        trader = GO.GetComponent<Trader>();
+        LeftCards = trader.traderList;
+        UpdateCard();
+    }
+
+    void Update()
+    {
+
+    }
+    void UpdateCard()
+    {
+        
+        for (int i = 0; i < LeftCards.Count(); i++)
+        {
+            cardPrefab[i].SetActive(true);
+        }
     }
 }
--- Assets/Scripts/Trader.cs
+++ Assets/Scripts/Trader.cs
@@ -51,7 +51,7 @@ public class Trader : MonoBehaviour
     public int counter = 0;
 
 
-    void Start()
+    void Awake()
     {
         traderList.Add(generateTraders());
     }
